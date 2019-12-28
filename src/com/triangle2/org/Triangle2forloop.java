f2b9aa2cdc928129384a9652f287ab4094d347ec
Mahamilky <mahamilky999@gmail.com>
2019-12-28 10:13:27 +0530

    second commit

---
+++ src/com/triangle2/org/Triangle2forloop.java
@@ -0,0 +1,15 @@
+package com.triangle2.org;
+
+public class Triangle2forloop {
+	public static void main(String[] args) {
+		int i=10;
+		for(i=3;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j);
+			
+		}
+		System.out.println();
+	}
+
+}
+}
